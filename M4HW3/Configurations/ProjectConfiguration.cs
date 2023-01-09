cced3827cf798a99f93ab76bdeee60972d964a13
Denys <86122979+Swartalfheim@users.noreply.github.com>
2023-01-09 18:35:31 +0200

    add code

---
+++ M4HW3/Configurations/ProjectConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using M4HW3.Configurats;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace M4HW3.Configurations
+{
+    public class ProjectConfiguration : IEntityTypeConfiguration<Project>
+    {
+        public void Configure(EntityTypeBuilder<Project> builder)
+        {
+            builder.ToTable("Project").HasKey(p => p.ProjectId);
+            builder.Property(p => p.ProjectId).ValueGeneratedOnAdd();
+            builder.Property(p => p.Name).IsRequired().HasMaxLength(50);
+            builder.Property(p => p.Budget).IsRequired().HasColumnType("money");
+            builder.Property(p => p.StartedDate).IsRequired();
+        }
+    }
+}
